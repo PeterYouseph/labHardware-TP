3e396e2715cd009a16adf16994df4b66cd5b9e6b
Luiz Carlos <123337482+Krakoziam@users.noreply.github.com>
2023-10-14 20:46:04 -0400

    Update de testbench

--- vhdl_testbenchs/codec_testbench.vhd
+++ vhdl_testbenchs/codec_testbench.vhd
@@ -4,54 +4,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity codec_tb is
 end entity codec_tb;
 
-architecture sim of codec_tb is
-    -- Declaração de sinais
-    signal input_data      : std_logic_vector(7 downto 0) := "00000000";
-    signal control_signal  : std_logic := '0';
-    signal output_data     : std_logic_vector(7 downto 0);
-    
-    -- Declaração do componente CODEC
-    component codec
-        port(
-            input_data     : in  std_logic_vector(7 downto 0);
-            control_signal : in  std_logic;
-            output_data    : out std_logic_vector(7 downto 0)
-        );
-    end component;
+architecture testbench of codec_tb is
+  -- Component declaration for the codec entity
+  component codec is
+    port
+    (
+      interrupt      : in std_logic;
+      read_signal    : in std_logic;
+      write_signal   : in std_logic;
+      valid          : out std_logic;
+      codec_data_in  : in std_logic_vector(7 downto 0);
+      codec_data_out : out std_logic_vector(7 downto 0)
+    );
+  end component;
 
+  -- Signals for the testbench
+  signal interrupt : std_logic := '0';
+  signal read_signal : std_logic := '0';
+  signal write_signal : std_logic := '0';
+  signal valid : std_logic;
+  signal codec_data_in : std_logic_vector(7 downto 0) := (others => '0');
+  signal codec_data_out : std_logic_vector(7 downto 0);
+  
+  -- File variables for input and output files
+  file input_file : file of bit;
+  file output_file : file of bit;
+  variable byte_buffer : bit_vector(7 downto 0);
+  
 begin
     -- Instância do componente CODEC
     UUT : codec
-        port map(
-            input_data     => input_data,
-            control_signal => control_signal,
-            output_data    => output_data
-        );
-
+        port map (
+            interrupt => interrupt,
+            read_signal => read_signal,
+            write_signal => write_signal,
+            valid => valid,
+            codec_data_in => codec_data_in,
+            codec_data_out => codec_data_out
+         );
+    
     -- Processo de clock
     process
     begin
-        -- Inicializações
-        input_data     <= "00000000"; -- Valor inicial para input_data
-        control_signal <= '0';       -- Valor inicial para control_signal
-
-        -- Teste 1
-        wait for 10 ns;
-        input_data     <= "11111111"; -- Configura o valor de input_data
-        control_signal <= '1';       -- Configura o valor de control_signal
-        wait for 10 ns;
-        -- Verifique os resultados, se necessário
-
-        -- Teste 2
-        wait for 10 ns;
-        input_data     <= "01010101"; -- Configura o valor de input_data
-        control_signal <= '0';       -- Configura o valor de control_signal
-        wait for 10 ns;
-        -- Verifique os resultados, se necessário
-
-        -- Continue adicionando testes conforme necessário
-
-        -- Encerra a simulação
+        -- terminar o test bench
         wait;
     end process;
-end architecture sim;
+end architecture;
