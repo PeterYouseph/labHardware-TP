d890c7586c408f707846f4f187b439d4bf1d10b9
Luiz Carlos <123337482+Krakoziam@users.noreply.github.com>
2023-10-23 19:13:32 -0400

    melhoria da indexão

--- vhdl_components/memory.vhd
+++ vhdl_components/memory.vhd
@@ -13,27 +13,30 @@ entity memory is
     clock      : in std_logic; -- Clock signal; Write on Falling-Edge
     data_read  : in std_logic; -- When '1', read data from memory
     data_write : in std_logic; -- When '1', write data to memory
-    data_addr  : in std_logic_vector(addr_width - 1 downto 0);
-    data_in    : in std_logic_vector((data_width * 2) - 1 downto 0);
-    data_out   : out std_logic_vector((data_width * 4) - 1 downto 0)
+    data_addr  : in std_logic_vector(addr_width - 1 downto 0); -- Address to read/write data
+    data_in    : in std_logic_vector((data_width * 2) - 1 downto 0);  -- Data to write to memory 
+    data_out   : out std_logic_vector((data_width * 4) - 1 downto 0) -- Data read from memory
   );
 end entity;
 
 architecture behavioral of memory is
-  type memory_array is array (natural range 0 to 2 ** addr_width - 1) of std_logic_vector(data_width - 1 downto 0);
-  signal mem : memory_array;
+
+  type memory_array is array (natural range 0 to 2 ** addr_width - 1) of std_logic_vector(data_width - 1 downto 0); -- Declaração do tipo de dado que será armazenado na memória
+
+  signal mem : memory_array; -- Declaração do sinal que será a memória
+
 begin
+
   process (clock)
   begin
-    if falling_edge(clock) then
+    if falling_edge(clock) then -- A memória só é escrita na borda de descida do clock
+  
       if data_write = '1' then
-        -- Write data to memory at the specified address
-        -- Escreve os dados na memória, conforme o endereço especificado
-        mem(to_integer(unsigned(data_addr))) <= data_in(data_width - 1 downto 0);
-      elsif data_read = '1' then
-        -- Read data from memory at the specified address
-        -- Lê os dados da memória, conforme o endereço especificado
-        data_out(data_width - 1 downto 0) <= mem(to_integer(unsigned(data_addr)));
+        mem(to_integer(unsigned(data_addr))) <= data_in(data_width - 1 downto 0); -- Escreve os dados na memória, conforme o endereço especificado
+
+      elsif data_read = '1' then 
+        data_out(data_width - 1 downto 0) <= mem(to_integer(unsigned(data_addr))); -- Lê os dados da memória, conforme o endereço especificado
+
       end if;
     end if;
   end process;
