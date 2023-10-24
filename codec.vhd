759f59716ce420f7dd78d18da1c76c96d2c958b9
Luiz Carlos <123337482+Krakoziam@users.noreply.github.com>
2023-10-24 19:27:35 -0400

    Colocado a logica de leitura

--- codec.vhd
+++ codec.vhd
@@ -1,7 +1,7 @@
-use std.textio.all;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
 
 entity codec is
   port
@@ -16,12 +16,14 @@ entity codec is
 end entity codec;
 
 architecture behavioral of codec is
-  signal data_buffer        : std_logic_vector(7 downto 0) := (others => '0');
-  signal is_data_valid      : boolean                      := false;
-  file input_file           : text open READ_MODE is "input.bin";
-  shared variable file_line : line;
-  shared variable file_data : integer := 0;
-  shared variable file_sum  : integer := 0;
+  signal data_buffer        : std_logic_vector(7 downto 0) := (others => '0'); -- Used to store the data to be read from the codec
+  signal is_data_valid      : boolean                      := false; -- Used to indicate if the data in the buffer is valid
+  file input_file           : text open READ_MODE is "input.bin"; -- Input file
+  file output_file          : text open WRITE_MODE is "output.bin"; -- Output file
+  shared variable file_line : line; -- Used to read from the input file 
+  shared variable file_data : integer := 0; -- Used to read from the input file
+  shared variable file_sum  : integer := 0; -- Used to calculate the sum of the input file
+
 begin
   process (interrupt, read_signal, write_signal, codec_data_in)
   begin
@@ -41,7 +43,13 @@ begin
           end if;
         end if;
       elsif write_signal = '1' then
+        -- Write to file
         if not is_data_valid then
+          writeline(output_file, file_line);
+          write(file_line, to_integer(unsigned(codec_data_in)));
+        end if;
+        
+        if endfile(output_file) then 
           data_buffer   <= codec_data_in;
           is_data_valid <= true;
         end if;
