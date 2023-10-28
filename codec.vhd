b151a1b3825e62a33d8313cd22f0267dd3b218d6
peteryouseph <peteryouseph@gmail.com>
2023-10-27 20:27:35 -0400

    fix: fixes codec reading and writing files

--- codec.vhd
+++ codec.vhd
@@ -26,47 +26,38 @@ entity codec is -- Declaração da entidade codec
 end entity codec;
 
 architecture behavioral of codec is -- Declaração da arquitetura comportamental do codec
-  signal data_buffer        : std_logic_vector(7 downto 0) := (others => '0'); -- Buffer de dados para armazenar os dados a serem lidos do codec
-  signal is_data_valid      : boolean                      := false; -- Sinal para indicar se os dados no buffer são válidos
-  file input_file           : text open READ_MODE is "input.bin"; -- Arquivo de entrada
-  file output_file          : text open WRITE_MODE is "output.bin"; -- Arquivo de saída
-  shared variable file_line : line; -- Variável para ler do arquivo de entrada 
-  shared variable file_data : integer := 0; -- Variável para ler do arquivo de entrada 
-  shared variable file_sum  : integer := 0; -- Variável para calcular a soma do arquivo de entrada 
+  signal data_buffer   : std_logic_vector(7 downto 0) := (others => '0'); -- Buffer de dados para armazenar os dados a serem lidos do codec
+  signal is_data_valid : boolean                      := false; -- Sinal para indicar se os dados no buffer são válidos
+  subtype logic is std_logic_vector(7 downto 0); -- Subtipo de std_logic_vector
+  type type_file is file of logic; -- Tipo de arquivo
+  file input_file           : type_file open READ_MODE is "input.bin"; -- Arquivos de entrada
+  file output_file          : type_file open WRITE_MODE is "output.bin"; -- Arquivos de saída
+  shared variable file_data : logic; -- Variável para ler do arquivo de entrada 
 
 begin
-  process (interrupt, read_signal, write_signal, codec_data_in) -- Processo que é sensível aos sinais interrupt, read_signal, write_signal e codec_data_in
+  process (interrupt, read_signal, write_signal, codec_data_in)
   begin
-    if interrupt = '1' then -- Se o sinal de interrupção for '1'
-      if read_signal = '1' then -- Se o sinal de leitura for '1'
-        if not is_data_valid then -- Se os dados no buffer não forem válidos
-          -- Leitura do arquivo
-          if not endfile(input_file) then -- Se não for o fim do arquivo de entrada
-            readline(input_file, file_line); -- Lê uma linha do arquivo de entrada
-            read(file_line, file_data); -- Lê os dados da linha do arquivo de entrada 
-            file_sum := file_sum + file_data; -- Calcula a soma dos dados do arquivo de entrada 
-          end if;
+    if interrupt = '1' then
+      if read_signal = '1' then -- Leitura do arquivo
 
-          if endfile(input_file) then -- Se for o fim do arquivo de entrada 
-            data_buffer   <= std_logic_vector(to_unsigned(file_sum, 8)); -- Armazena a soma dos dados do arquivo de entrada no buffer de dados 
-            is_data_valid <= true; -- Define que os dados no buffer são válidos 
-          end if;
-        end if;
-      elsif write_signal = '1' then -- Se o sinal de escrita for '1'
-        -- Escrita no arquivo 
-        if not is_data_valid then -- Se os dados no buffer não forem válidos 
-          for cont_loop in 0 to 7 loop -- Loop para cada bit no byte 
-            write(file_line, integer'image(to_integer(unsigned'("0" & codec_data_in(cont_loop))))); -- Escreve cada bit no byte no arquivo de saída 
-          end loop;
-          writeline(output_file, file_line); -- Escreve a linha no arquivo de saída 
-          is_data_valid <= true; -- Define que os dados no buffer são válidos 
-        end if;
+        while not endfile(input_file) loop -- Enquanto não chegar ao fim do arquivo
+          read(input_file, file_data);
+          codec_data_out <= file_data;
+        end loop;
+        is_data_valid <= true;
+
+      elsif write_signal = '1' then -- Escrita no arquivo 
+
+        for i in 0 to 7 loop
+          write(output_file, codec_data_in); -- Escreve cada bit no byte no arquivo de saída
+        end loop;
+        is_data_valid <= true; -- Define que os dados no buffer são válidos
+      else
+        is_data_valid <= false;
       end if;
     end if;
-
     if is_data_valid then -- Se os dados no buffer forem válidos 
-      codec_data_out <= data_buffer; -- Define que os dados lidos do codec são os dados no buffer 
-      valid          <= '1'; -- Define que os dados são válidos 
+      valid <= '1'; -- Define que os dados são válidos 
     else
       valid <= '0'; -- Define que os dados não são válidos 
     end if;
