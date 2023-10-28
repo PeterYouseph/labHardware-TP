912d7bde8cf5e38d899acdc56b1750a45df49d4e
peteryouseph <peteryouseph@gmail.com>
2023-10-28 13:48:44 -0400

    doc: include comments in memory_test

--- memory_test.vhd
+++ memory_test.vhd
@@ -1,3 +1,11 @@
+-- Definição de Sinais: Os sinais interrupt, read_signal, write_signal, valid, codec_data_in e codec_data_out são definidos. Esses sinais serão usados para interagir com o componente codec.
+
+-- Instanciação do Componente Codec: O componente codec é instanciado com os sinais definidos conectados aos seus respectivos portos.
+
+-- Teste de Operação de Escrita: Um processo é iniciado para testar a operação de escrita do codec. O sinal de interrupção é ativado, o sinal de escrita é ativado e um exemplo de dados é fornecido ao codec_data_in. Após um tempo simulado, o sinal de escrita e os dados são resetados. O sinal valid é então verificado para garantir que a operação de escrita foi bem-sucedida.
+
+-- Teste de Operação de Leitura: Um segundo processo é iniciado para testar a operação de leitura do codec. O sinal de interrupção é ativado e o sinal de leitura é ativado. Após um tempo simulado, o sinal valid é verificado para garantir que a operação de leitura foi bem-sucedida.
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
